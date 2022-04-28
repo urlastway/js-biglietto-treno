55b900b2c9644e795d65e647b4a72ac0b8a2ab85
urlastway <andriy.semenchenko13@gmail.com>
2022-04-28 17:29:30 +0200

    com1

--- main.js
+++ main.js
@@ -5,14 +5,11 @@ const km = Number(prompt("Quanti km vuoi percorrere?"))
 //Sale over 65
 const price = 0.21
 let sumprice = km * price
-if (age > 18){
-    sumprice = sumprice - (sumprice / 100 * 20)
+if (age < 18){
+    sumprice = sumprice - (sumprice * 20 / 100)
 }
-else if (age <= 65){
-    sumprice = sumprice - (sumprice / 100 * 40)
-}
-else {
-    sumprice = sumprice
+else if (age >= 65){
+    sumprice = sumprice - (sumprice * 40 / 100)
 }
 
-console.log(sumprice)
+console.log(sumprice.toFixed(2))
